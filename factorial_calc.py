47a831929cb65ad13c60401ca5f5af0de306021a
Deanomatic <jonathdsmith19@gmail.com>
2020-04-25 22:34:45 -0500

    removing comments

--- factorial_calc.py
+++ factorial_calc.py
@@ -1,8 +1,3 @@
-# Nathan Smith
-# Section: 005
-# Assignment: OLA6
-# This program takes a positive integer as an input and give its factorial
-
 def main():
     number = int(input('Enter a non-negative integer: '))
     if number < 0:
